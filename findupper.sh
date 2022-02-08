b28a5fb5c7759537cc8bbc0ab99c602139bc1d32
Amir Hossam <amir_h56395@cic-cairo.com>
2022-02-08 15:41:15 +0200

    Task1

---
+++ findupper.sh
@@ -0,0 +1,25 @@
+echo "enter your charcter:"
+read  var
+
+case $var  in
+
+        +([[:upper:]]) )
+         echo    "upper case "                                    
+                ;;
+                #same way to match one char
+        +([[:lower:]]) )
+        echo   "low case "          
+                ;;
+        +([[:alpha:]]) )
+                echo "mix char upper and lower"
+                ;;
+        +([[:digit:]]) )
+        echo    "integer "                                     
+        ;;
+        +([[:alnum:]]) )
+                echo "mix int and upper and lower"
+                ;;
+        *)
+        echo wrong data
+        ;;
+esac
